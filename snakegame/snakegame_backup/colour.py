8e7b8885f5aeaeb22360ef10c5517e121e753b86
Matt Stark <linuxisawesome@gmail.com>
2012-11-01 07:23:12 +1100

    removed the backup files from repo

--- snakegame/snakegame_backup/colour.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-def hash_colour(data):
-    data = map(ord, hashlib.md5(data).digest())
-    colour = data[::3], data[1::3], data[2::3]
-    colour = map(sum, colour)
-    return (colour[0] % 255, colour[1] % 255, colour[2] % 255)
-
